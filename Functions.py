f4ead853adf55370833b12e6027d649e2049a148
Eden <edench@post.bgu.ac.il>
2021-05-11 20:55:50 +0300

    Creates all functions

--- Functions.py
+++ Functions.py
@@ -1,45 +1,131 @@
-# Define an objective function, for example above:
-# Example from the documentations on how to create a function for the model
-def Fx(solution):
-    fx = solution[0] ** 3 + solution[1] ** 2 + solution[2] ** 4
-    return fx
+import math
+import random
+from math import cos, sin, sqrt, exp, e
+
+import numpy as np
+from numpy import pi
 
 
 # Arie's example of simple rosenbrock function
-def RosenbrockSimple(solution):
-    return 100 * (solution[0] ** 2 - solution[1]) ** 2 + (1 - solution[0]) ** 2
+def RosenbrockSimple(X):
+    fx = 100 * (X[0] ** 2 - X[1]) ** 2 + (1 - X[0]) ** 2
+    return fx
 
 
 ##################################################
 # TODO EDEN
 #########################################
-def Sphere(solution):
-    return
+
+# Search range: [-100,100]
+# Initialization range: [-100,50]
+def Sphere(X):
+    fx = 0.0
+    for i in range(10):
+        fx += X[i] ** 2
+    return fx
 
 
-def Rosenbrock(solution):
-    return
+# Search range: [-2.048,2.048]
+# Initialization range: [-2.048,2.048]
+def Rosenbrock(X):
+    fx = 0.0
+    for i in range(9):
+        fx = + 100 * pow((pow(X[i], 2) - X[i + 1]), 2) + pow((1 - X[i]), 2)
+    return fx
 
 
-def Ackley(solution):
-    return
+# Search range: [-32.768,32.768]
+# Initialization range: [-32.768,16]
+def Ackley(X):
+    sygma_x_2 = 0.0
+    sygma_cos = 0.0
+    for i in range(10):
+        sygma_x_2 += pow(X[i], 2)
+        sygma_cos += cos(2 * pi * X[i])
+    
+    fx = -20 * exp(-0.2 * sqrt(0.1 * sygma_x_2)) - exp(0.1 * sygma_cos) + 20 + e
+    return fx
 
 
-def Griewank(solution):
-    return
+# Search range: [-600,600]
+# Initialization range: [-600,200]
+def Griewank(X):
+    part1 = 0.0
+    part2 = 1.0
+    for i in range(10):
+        part1 += X[i] ** 2
+        part2 *= cos(float(X[i]) / sqrt(i + 1))
+    fx = (float(part1) / 4000.0) - float(part2) + 1
+    return fx
 
 
-def Weierstrass(solution):
-    return
+# Search range: [-0.5,0.5]
+# Initialization range: [-0.5,0.2]
+def Weierstrass(X):
+    part1 = 0.0
+    part2 = 0.0
+    k_max = 21
+    for i in range(10):
+        for k in range(k_max):
+            part1 += pow(0.5, k) * cos(2 * pi * pow(3, k) * (X[i] + 0.5))
+    for k in range(k_max):
+        part2 += pow(0.5, k) * cos(2 * pi * pow(3, k) * 0.5)
+    fx = part1 - 10 * part2
+    return fx
 
 
-def Rastrigin(solution):
-    return
+# Search range: [−5.12, 5.12]
+# Initialization range: [−5.12, 2]
+def Rastrigin(X):
+    fx = 0.0
+    for i in range(10):
+        fx += pow(X[i], 2) - 10 * cos(2 * pi * X[i])
+    return fx
 
 
-def NCRastrigin(solution):
-    return
+# Search range: [−5.12, 5.12]
+# Initialization range: [−5.12, 2]
+def NCRastrigin(X):
+    fx = 0.0
+    Y = [0.0]*10
+    for i in range(10):
+        if abs(X[i]) < 0.5:
+            Y[i] = X[i]
+        else:
+            Y[i] = round(2 * X[i])/2
+    for i in range(10):
+        fx += pow(Y[i], 2) - 10 * cos(2 * pi * Y[i]) + 10
+    return fx
 
 
-def Schwefel(solution):
-    return
+# Search range: [−500, 500]
+# Initialization range: [−500, 500]
+def Schwefel(X):
+    fx = 0.0
+    for i in range(10):
+        fx += X[i] * sin(sqrt(abs(X[i])))
+    fx = (-1) * fx
+    return fx
+
+X = []
+for i in range(10):
+    X.append(random.randint(-500,500))
+# X = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X2 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X3 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X4 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X5 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X6 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X7 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X8 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# X9 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+print(X)
+print(RosenbrockSimple(X))
+print(Sphere(X))
+print(Rosenbrock(X))
+print(Ackley(X))
+print(Griewank(X))
+print(Weierstrass(X))
+print(Rastrigin(X))
+print(NCRastrigin(X))
+print(Schwefel(X))
--- Main.py
+++ Main.py
@@ -1,9 +1,3 @@
-# import pip
-#
-# package_names=['mealpy'] #packages to install
-# pip.main(['install'] + package_names + ['--upgrade'])
-# # --upgrade to install or update existing packages
-#################################################################
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,27 +41,29 @@ from opfunu.cec_basic.cec2014_nobias import *
 from mealpy.math_based.HC import OriginalHC, BaseHC
 from mealpy.swarm_based.GWO import BaseGWO, RW_GWO
 import Functions
+
 ###############################################
-#TODO ARIE
+# TODO ARIE
 
 # Setting parameters
 # obj_func = F5       # This objective function come from "opfunu" library. You can design your own objective function like above
 # verbose = False     # Print out the training results
 # epoch = 500         # Number of iterations / generations / epochs
 # pop_size = 50       # Populations size (Number of individuals / Number of solutions)
-obj_func = Functions.RosenbrockSimple
-verbose = False
+obj_func = Functions.Rosenbrock
+verbose = True
 epoch = 10
 pop_size = 100
 
-problemSize = 2
+problemSize = 10
 lb2 = -10
 ub2 = 10
-#Remember the keyword "problem_size"
+
+# Remember the keyword "problem_size"
 # md2 = BaseHC(obj_func, lb2, ub2, verbose, epoch, pop_size,problem_size=problemSize)
 # best_pos1, best_fit1, list_loss1 = md2.train()
 # print(md2.solution[1])
 
-md2 = BaseGWO(obj_func, lb2, ub2, verbose, epoch, pop_size, problem_size=problemSize)
+md2 = BaseGWO(obj_func, lb2, ub2, verbose, epoch, pop_size, problem_size = problemSize)
 best_pos1, best_fit21, list_loss1 = md2.train()
 print(md2.solution[1])
