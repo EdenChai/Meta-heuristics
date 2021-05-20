de328956c2a95e86619406b5ac56297a0f50c1b7
Eden <edench@post.bgu.ac.il>
2021-05-20 12:41:31 +0300

    Finish

--- Functions.py
+++ Functions.py
@@ -1,41 +1,24 @@
-import math
-import random
 from math import cos, sin, sqrt, exp, e
-
-import numpy as np
 from numpy import pi
 
+""" This class contains all functions from the article """
 
-# Arie's example of simple rosenbrock function
-def RosenbrockSimple(X):
+def Rosenbrock2D(X):
     fx = 100 * (X[0] ** 2 - X[1]) ** 2 + (1 - X[0]) ** 2
     return fx
 
-
-##################################################
-# TODO EDEN
-#########################################
-
-# Search range: [-100,100]
-# Initialization range: [-100,50]
 def Sphere(X):
     fx = 0.0
     for i in range(10):
         fx += X[i] ** 2
     return fx
 
-
-# Search range: [-2.048,2.048]
-# Initialization range: [-2.048,2.048]
-def Rosenbrock(X):
+def Rosenbrock10D(X):
     fx = 0.0
     for i in range(9):
-        fx = + 100 * pow((pow(X[i], 2) - X[i + 1]), 2) + pow((1 - X[i]), 2)
+        fx += 100 * pow((pow(X[i], 2) - X[i + 1]), 2) + pow((1 - X[i]), 2)
     return fx
 
-
-# Search range: [-32.768,32.768]
-# Initialization range: [-32.768,16]
 def Ackley(X):
     sygma_x_2 = 0.0
     sygma_cos = 0.0
@@ -46,9 +29,6 @@ def Ackley(X):
     fx = -20 * exp(-0.2 * sqrt(0.1 * sygma_x_2)) - exp(0.1 * sygma_cos) + 20 + e
     return fx
 
-
-# Search range: [-600,600]
-# Initialization range: [-600,200]
 def Griewank(X):
     part1 = 0.0
     part2 = 1.0
@@ -58,9 +38,6 @@ def Griewank(X):
     fx = (float(part1) / 4000.0) - float(part2) + 1
     return fx
 
-
-# Search range: [-0.5,0.5]
-# Initialization range: [-0.5,0.2]
 def Weierstrass(X):
     part1 = 0.0
     part2 = 0.0
@@ -73,33 +50,24 @@ def Weierstrass(X):
     fx = part1 - 10 * part2
     return fx
 
-
-# Search range: [−5.12, 5.12]
-# Initialization range: [−5.12, 2]
 def Rastrigin(X):
     fx = 0.0
     for i in range(10):
         fx += pow(X[i], 2) - 10 * cos(2 * pi * X[i])
     return fx
 
-
-# Search range: [−5.12, 5.12]
-# Initialization range: [−5.12, 2]
 def NCRastrigin(X):
     fx = 0.0
-    Y = [0.0]*10
+    Y = [0.0] * 10
     for i in range(10):
         if abs(X[i]) < 0.5:
             Y[i] = X[i]
         else:
-            Y[i] = round(2 * X[i])/2
+            Y[i] = round(2 * X[i]) / 2
     for i in range(10):
         fx += pow(Y[i], 2) - 10 * cos(2 * pi * Y[i]) + 10
     return fx
 
-
-# Search range: [−500, 500]
-# Initialization range: [−500, 500]
 def Schwefel(X):
     fx = 0.0
     for i in range(10):
@@ -107,25 +75,139 @@ def Schwefel(X):
     fx = (-1) * fx
     return fx
 
-X = []
-for i in range(10):
-    X.append(random.randint(-500,500))
-# X = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X2 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X3 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X4 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X5 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X6 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X7 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X8 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-# X9 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
-print(X)
-print(RosenbrockSimple(X))
-print(Sphere(X))
-print(Rosenbrock(X))
-print(Ackley(X))
-print(Griewank(X))
-print(Weierstrass(X))
-print(Rastrigin(X))
-print(NCRastrigin(X))
-print(Schwefel(X))
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+
+# from math import cos, sin, sqrt, exp, e
+# from numpy import pi
+#
+# # Search range: [-10,10]
+# def Rosenbrock2D(X):
+#     fx = 100 * (X[0] ** 2 - X[1]) ** 2 + (1 - X[0]) ** 2
+#     return fx
+#
+# # Search range: [-100,100]
+# # Initialization range: [-100,50]
+# def Sphere(X):
+#     fx = 0.0
+#     for i in range(10):
+#         fx += X[i] ** 2
+#     return fx
+#
+#
+# # Search range: [-2.048,2.048]
+# # Initialization range: [-2.048,2.048]
+# def Rosenbrock10D(X):
+#     fx = 0.0
+#     for i in range(9):
+#         fx = + 100 * pow((pow(X[i], 2) - X[i + 1]), 2) + pow((1 - X[i]), 2)
+#     return fx
+#
+#
+# # Search range: [-32.768,32.768]
+# # Initialization range: [-32.768,16]
+# def Ackley(X):
+#     sygma_x_2 = 0.0
+#     sygma_cos = 0.0
+#     for i in range(10):
+#         sygma_x_2 += pow(X[i], 2)
+#         sygma_cos += cos(2 * pi * X[i])
+#
+#     fx = -20 * exp(-0.2 * sqrt(0.1 * sygma_x_2)) - exp(0.1 * sygma_cos) + 20 + e
+#     return fx
+#
+#
+# # Search range: [-600,600]
+# # Initialization range: [-600,200]
+# def Griewank(X):
+#     part1 = 0.0
+#     part2 = 1.0
+#     for i in range(10):
+#         part1 += X[i] ** 2
+#         part2 *= cos(float(X[i]) / sqrt(i + 1))
+#     fx = (float(part1) / 4000.0) - float(part2) + 1
+#     return fx
+#
+#
+# # Search range: [-0.5,0.5]
+# # Initialization range: [-0.5,0.2]
+# def Weierstrass(X):
+#     part1 = 0.0
+#     part2 = 0.0
+#     k_max = 21
+#     for i in range(10):
+#         for k in range(k_max):
+#             part1 += pow(0.5, k) * cos(2 * pi * pow(3, k) * (X[i] + 0.5))
+#     for k in range(k_max):
+#         part2 += pow(0.5, k) * cos(2 * pi * pow(3, k) * 0.5)
+#     fx = part1 - 10 * part2
+#     return fx
+#
+#
+# # Search range: [−5.12, 5.12]
+# # Initialization range: [−5.12, 2]
+# def Rastrigin(X):
+#     fx = 0.0
+#     for i in range(10):
+#         fx += pow(X[i], 2) - 10 * cos(2 * pi * X[i])
+#     return fx
+#
+#
+# # Search range: [−5.12, 5.12]
+# # Initialization range: [−5.12, 2]
+# def NCRastrigin(X):
+#     fx = 0.0
+#     Y = [0.0]*10
+#     for i in range(10):
+#         if abs(X[i]) < 0.5:
+#             Y[i] = X[i]
+#         else:
+#             Y[i] = round(2 * X[i])/2
+#     for i in range(10):
+#         fx += pow(Y[i], 2) - 10 * cos(2 * pi * Y[i]) + 10
+#     return fx
+#
+#
+# # Search range: [−500, 500]
+# # Initialization range: [−500, 500]
+# def Schwefel(X):
+#     fx = 0.0
+#     for i in range(10):
+#         fx += X[i] * sin(sqrt(abs(X[i])))
+#     fx = (-1) * fx
+#     return fx
+#
+# # X = []
+# # for i in range(10):
+# #     X.append(random.randint(-500,500))
+# # X = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X2 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X3 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X4 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X5 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X6 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X7 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X8 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # X9 = [1, 0.5, 3.8, 25.111, 5, 15.65, -20.78, 52.55, 44, 0]
+# # print(X)
+# # print(RosenbrockSimple(X))
+# # print(Sphere(X))
+# # print(Rosenbrock(X))
+# # print(Ackley(X))
+# # print(Griewank(X))
+# # print(Weierstrass(X))
+# # print(Rastrigin(X))
+# # print(NCRastrigin(X))
+# # print(Schwefel(X))
--- Main.py
+++ Main.py
@@ -1,69 +1,149 @@
-# Imports
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import preprocessing
-
-# # Data
-# x = np.linspace(-10, 10, 20)
-# y = np.linspace(-10, 10, 20)
-# X, Y = np.meshgrid(x,y)
-#
-# # Multivariate Normal
-# mu_x = np.mean(x)
-# sigma_x = np.std(x)
-# mu_y = np.mean(y)
-# sigma_y = np.std(y)
-#
-#
-# rv = multivariate_normal([mu_x, mu_y], [[sigma_x, 0], [0, sigma_y]])
-#
-# normalized = preprocessing.normalize(rv.pdf)
-# # Probability Density
-# pos = np.empty(X.shape + (2,))
-# pos[:, :, 0] = X
-# pos[:, :, 1] = Y
-# pd = rv.pdf(pos)
-#
-# # Plot
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.plot_surface(X, Y, pd, cmap='viridis', linewidth=0)
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Probability Density')
-# plt.title("Multivariate Normal Distribution")
-# plt.show()
-#####################################################################################
-from opfunu.cec_basic.cec2014_nobias import *
-from opfunu.cec_basic.cec2014_nobias import *
-from mealpy.math_based.HC import OriginalHC, BaseHC
-from mealpy.swarm_based.GWO import BaseGWO, RW_GWO
+from mealpy.math_based.HC import BaseHC
+from mealpy.physics_based.SA import BaseSA
+from mealpy.swarm_based.ABC import BaseABC
+from mealpy.swarm_based.EHO import BaseEHO
+from mealpy.swarm_based.GWO import BaseGWO
+from mealpy.swarm_based.PSO import BasePSO
+from mealpy.swarm_based.WOA import BaseWOA
 import Functions
 
-###############################################
-# TODO ARIE
+function = Functions.Rosenbrock2D       # This objective function come from "opfunu" library.
+verbose = True                          # Print out the training results
+epoch = 10                              # Number of iterations / epochs
+populationSize = 10                     # Populations size (Number of individuals / Number of solutions)
+problemSize = 2                         # Number of dimensions
+lowerBound = [-10]                      # The lower bound of the function
+upperBound = [10]                       # The upper bound of the function
 
-# Setting parameters
-# obj_func = F5       # This objective function come from "opfunu" library. You can design your own objective function like above
-# verbose = False     # Print out the training results
-# epoch = 500         # Number of iterations / generations / epochs
-# pop_size = 50       # Populations size (Number of individuals / Number of solutions)
-obj_func = Functions.Rosenbrock
-verbose = True
-epoch = 10
-pop_size = 100
+model = BaseHC(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+best_pos, best_fit, list_loss = model.train()
 
-problemSize = 10
-lb2 = -10
-ub2 = 10
+"""" Stage 1-b """
 
-# Remember the keyword "problem_size"
-# md2 = BaseHC(obj_func, lb2, ub2, verbose, epoch, pop_size,problem_size=problemSize)
-# best_pos1, best_fit1, list_loss1 = md2.train()
-# print(md2.solution[1])
+# Run HC first time
+print("Run HC first time on 2D Rosenbrock")
+print(model.solution[1])
+print(model.loss_train)
 
-md2 = BaseGWO(obj_func, lb2, ub2, verbose, epoch, pop_size, problem_size = problemSize)
-best_pos1, best_fit21, list_loss1 = md2.train()
-print(md2.solution[1])
+# Run HC second time
+print("\nRun HC second time on 2D Rosenbrock")
+model.train()
+print(model.solution[1])
+
+##################################################################
+
+"""" Stage 1-c """
+
+# Run GWO first time
+print("\nRun GWO first time on 2D Rosenbrock")
+model = BaseGWO(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+model.train()
+print(model.solution[1])
+
+# Run GWO second time
+print("\nRun GWO second time on 2D Rosenbrock")
+model.train()
+print(model.solution[1])
+
+#################################################################
+
+"""" Stage 1-d """
+
+# Run both HC and GWO on 10D problem 50 times with different bounds
+
+function = Functions.Rosenbrock10D      # This objective function come from "opfunu" library.
+verbose = True                          # Print out the training results
+epoch = 50                              # Number of iterations / epochs
+populationSize = 10                     # Populations size (Number of individuals / Number of solutions)
+problemSize = 10                        # Number of dimensions
+lowerBound = [-2.048]                   # The lower bound of the function
+upperBound = [2.048]                    # The upper bound of the function
+
+# Run HC
+print("\nRun HC on 10D Rosenbrock")
+model = BaseHC(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+model.train()
+print(model.solution[1])
+
+# Run GWO
+print("\nRun GWO on 10D Rosenbrock")
+model = BaseGWO(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+model.train()
+print(model.solution[1])
+
+
+#################################################################
+
+"""" Stage 2-b """
+
+def runAlgorithms(func, lower, upper):
+    function = func
+    verbose = True
+    epoch = 10
+    populationSize = 10
+    problemSize = 10
+    lowerBound = lower
+    upperBound = upper
+    
+    # Run HC
+    model1 = BaseHC(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model1.train()
+    print(model1.solution[1])
+    
+    # Run SA
+    model2 = BaseSA(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model2.train()
+    print(model2.solution[1])
+    
+    # Run PSO
+    model3 = BasePSO(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model3.train()
+    print(model3.solution[1])
+    
+    # Run EHO
+    model4 = BaseEHO(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model4.train()
+    print(model4.solution[1])
+    
+    # Run WOA
+    model5 = BaseWOA(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model5.train()
+    print(model5.solution[1])
+    
+    # Run GWO
+    model6 = BaseGWO(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model6.train()
+    print(model6.solution[1])
+    
+    # Run ABC
+    model7 = BaseABC(function, lowerBound, upperBound, verbose, epoch, populationSize, problem_size = problemSize)
+    model7.train()
+    print(model7.solution[1])
+
+# runAlgorithms( Functions.Sphere, -100, 100 )
+# runAlgorithms( Functions.Rosenbrock10D, -2.048, 2.048 )
+# runAlgorithms( Functions.Ackley, -32.768, 32.76 )
+# runAlgorithms( Functions.Griewank, -600, 600 )
+# runAlgorithms( Functions.Weierstrass, -0.5, 0.5 )
+# runAlgorithms( Functions.Rastrigin, -5.12, 5.12 )
+# runAlgorithms( Functions.NCRastrigin, -5.12, 5.12 )
+# runAlgorithms( Functions.Schwefel, -500, 500 )
+
+# b = 10;
+# f = lambda x,y: (x-1)**2 + b*(y-x**2)**2;
+# # Initialize figure
+# figRos = plt.figure(figsize=(12, 7))
+# axRos = figRos.gca(projection='3d')
+#
+# # Evaluate function
+# X = np.arange(-10, 10, 0.15)
+# Y = np.arange(-10, 10, 0.15)
+# X, Y = np.meshgrid(X, Y)
+# Z = f(X,Y)
+#
+# # Plot the surface
+# surf = axRos.plot_surface(X, Y, Z, cmap=cm.gist_heat_r,
+#                        linewidth=0, antialiased=False)
+# axRos.set_zlim(0, 200)
+# figRos.colorbar(surf, shrink=0.5, aspect=10)
+# plt.show()
